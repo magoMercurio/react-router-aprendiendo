fe836ab499f4652ff2fa10e73e8d48b85a91b79b
mago_d_Mercurio <davila.feria@gmail.com>
2023-04-16 10:11:54 -0500

    add Page404

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import './assets/css/base/base.css';
 import './assets/css/componentes/card.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home.jsx'
+import Sobre from './pages/Sobre.jsx'
+import Page404 from "./pages/page404";
 
 function App() {
-  return (
-    <>
 
-    </>
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path='/' 
+          element={<Home/>}
+        />
+        <Route
+          path='/sobre'
+          element={<Sobre/>}
+        />
+        <Route
+          path='*'
+          element={<Page404/>}
+        />
+      </Routes>
+    </Router>
   );
 }
 
