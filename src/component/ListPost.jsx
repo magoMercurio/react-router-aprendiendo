88c7b9e969cc0dca8c14ec94ae553efb74f80aa0
mago_d_Mercurio <davila.feria@gmail.com>
2023-04-16 14:07:15 -0500

    add Post.jsx page

--- src/component/ListPost.jsx
+++ src/component/ListPost.jsx
@@ -1,9 +1,31 @@
 import '../assets/css/componentes/card.css'
+import { useState, useEffect } from "react"
+import { buscar } from '../api/api'
+import { Link } from "react-router-dom";
+
+
 
 const ListPosts = ({ url }) => {
+  const [posts, setPosts] = useState([])
 
-  return ( <section className='post container'>
-    
+  useEffect (() => {
+    buscar(url, setPosts)
+  }, [url])
+
+  return ( <section className='posts container'>
+    {
+    posts.map(post => {
+      const { id, title, metadescription, categoria } = post;
+      return <Link to={`/posts/${id}`} className={`post__card post-card--${categoria}`} key={id}>
+        <article >
+          <h3 className="post-card__title">
+            {title}
+          </h3>
+          <p className="post-card__meta">{metadescription}</p>
+        </article>
+      </Link>
+    })
+  }
     </section>
   )
 }
